dbde06ffc86ee6059079568911708a587fc608d1
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-28 17:31:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
 stringArray = string.split(" ")
 reversedarray= []
 stringArray.collect do |word|
-  reversedarray << word.pop
+  reversedarray << word.reverse
   reversedarray.join
 end 
 puts reversedarray
