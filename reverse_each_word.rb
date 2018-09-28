5ccdc3f1adc0de2c54d5c7d3eee67efd6da4fe96
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-28 17:33:12 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+stringArray = string.split(" ")
+reversedarray= []
+stringArray.collect do |word|
+  reversedarray << word.reverse
+  
+end 
+return reversedarray.join(" ")
+end 
+  
